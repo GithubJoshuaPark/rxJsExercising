2a2b4c495f3b92fb7863f6b12926224fac1d8fbb
JoshuaPark <soromiso@gmail.com>
2023-03-06 23:12:51 +0900

    the first commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+### exercising rxjs
+> at first I want to use capacitor Storage so installed
+```javascript
+  $ionic start rxjsExOnIonicAngular --type=angular
+  $cd rxjsExOnIonicAngular
+  $npm i rxjs
+  $npm i @capacitor/core @capacitor/cli
+  $npm i @capacitor/storage  --> deprecated --> @capacitor/preferences
+```
+
+> implenent in home.ts
+>
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,133 @@
 import { Component } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
+import { forkJoin, map, Observable, zip } from 'rxjs';
+import { of, from } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+interface Post {
+  id: number;
+  title: string;
+  author?: string;
+  body: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  {
 
-  constructor() {}
+  inputValue = '';
+  storedValue: string | null = '';
+  myObservable : any;
+
+  posts: Post[] = [];
+
+  items: {label: string, no: number}[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.myObservable = new Observable(observer => {
+      // This creates an observable that emits the values 1, 2, and 3, and then completes
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+
+    const mappedObservable = this.myObservable.pipe(map((value: number) => value * 2));
+    mappedObservable.subscribe((value: any) => console.log(value));
+
+    this.items = [].constructor(10).fill('label').map((label: string, index: number) => ({label: label + ' (' + index + ')', no: index}));
+  }
+
+  ngOnInit() {
+    this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts').subscribe(posts => {
+      this.posts = posts;
+    });
+  }
+
+  async handleSave() {
+    await Preferences.set({
+      key: 'myValue',
+      value: this.inputValue
+    });
+    this.inputValue = '';
+    await this.handleLoad();
+  }
+
+  async handleLoad() {
+    const { value } = await Preferences.get({ key: 'myValue' });
+    this.storedValue = value;
+  }
+
+  /**
+   * of() and from() are two static methods of the Observable class.
+   */
+  showOfAndFromEx() {
+    const myArray = [1, 2, 3];
+
+    const myObservable1 = of(myArray);
+    myObservable1.subscribe(value => console.log(value, 'of')); // emited all at once
+
+    const myObservable2 = from(myArray);
+    myObservable2.subscribe(value => console.log(value, 'from')); // emited one by one
+  }
+
+  onClick(item: number) {
+    console.log(item);
+
+    switch (item) {
+      case 0:
+        this.callForkJoinExample();
+        break;
+      case 1:
+        this.callZipExample();
+        break;
+      default:
+        break;
+    }
+  }
+
+  /**
+   * forkJoin() is a static method of the Observable class.
+   */
+  callForkJoinExample() {
+    console.log('callForkJoinExample');
+
+    const userObservable = this.http.get<User>('https://jsonplaceholder.typicode.com/users/1');
+    const postObservable = this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts?userId=1');
+
+    forkJoin([userObservable, postObservable]).subscribe(([user, posts]) => {
+      // using the forkJoin operator to combine these two observables
+      // into a single observable that emits both values as an array.
+      this.posts = posts.map((post, index) => ({id: index, title: post.title, author: user.name, body: 'body' }));
+    });
+  }
+
+  /**
+   *  zip() is a static method of the Observable class.
+   */
+  callZipExample() {
+    console.log('callForkJoinExample');
+
+    const userObservable = this.http.get<User>('https://jsonplaceholder.typicode.com/users/1');
+    const postObservable = this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts?userId=1');
+
+    zip([userObservable, postObservable]).subscribe(([user, posts]) => {
+      // using the zip operator to combine these two observables into a single observable
+      // that emits an array containing the latest values from both observables.
+      this.posts = posts.map((post, index) => ({id: index, title: post.title, author: user.name, body: 'body' }));
+    });
+  }
+
+
 
 }
