34bf9634f5c2ea4fe59948e21b78bfa4b1ab72f4
JoshuaPark <soromiso@gmail.com>
2023-03-12 17:02:31 +0900

    added subject, behaviorSubject, replaySubject'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Preferences } from '@capacitor/preferences';
-import { combineLatest, filter, forkJoin, map, merge, Observable, Subject, switchMap, takeUntil, zip } from 'rxjs';
+import { BehaviorSubject, combineLatest, filter, forkJoin, map, merge, Observable, ReplaySubject, Subject, Subscription, switchMap, takeUntil, zip } from 'rxjs';
 import { of, from } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { LoadingController } from '@ionic/angular';
@@ -30,9 +30,19 @@ export class HomePage implements OnDestroy{
   posts: Post[] = [];
   users: User[] = [];
 
+  mySubject = new Subject();
+  howManyCalled = 0;
+
+  myBehaviorSubject = new BehaviorSubject('initial value');
+  howManyCalledBehaviorSubject = 0;
+
+  myReplaySubject = new ReplaySubject(3);
+  howManyCalledReplaySubject = 0;
+
   items: { label: string; no: number }[] = [];
 
   private destroy = new Subject();
+  private subscriptions: Subscription[] = [];
 
   constructor(
               private http: HttpClient,
@@ -73,6 +83,11 @@ export class HomePage implements OnDestroy{
     //Add 'implements OnDestroy' to the class.
     this.destroy.next(0);
     this.destroy.complete();
+
+    this.mySubject.next
+    this.mySubject.complete();
+
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 
   async handleSave() {
@@ -96,10 +111,15 @@ export class HomePage implements OnDestroy{
     const myArray = [1, 2, 3];
 
     const myObservable1 = of(myArray);
-    myObservable1.subscribe((value) => console.log(value, 'of')); // emited all at once
+    this.subscriptions.push(
+      myObservable1.subscribe((value) => console.log(value, 'of')) // emited all at once
+    );
 
     const myObservable2 = from(myArray);
-    myObservable2.subscribe((value) => console.log(value, 'from')); // emited one by one
+    this.subscriptions.push(
+      myObservable2.subscribe((value) => console.log(value, 'from')) // emited one by one
+    );
+
   }
 
   /**
@@ -118,16 +138,19 @@ export class HomePage implements OnDestroy{
     this.posts = [];
     const loadingCtr = await this.showLoading('');
     try {
-      forkJoin([userObservable, postObservable]).subscribe(([user, posts]) => {
-        // using the forkJoin operator to combine these two observables
-        // into a single observable that emits both values as an array.
-        this.posts = posts.map((post, index) => ({
-          id: index,
-          title: post.title,
-          author: user.name,
-          body: 'body',
-        }));
-      });
+      this.subscriptions.push(
+        forkJoin([userObservable, postObservable]).subscribe(([user, posts]) => {
+          // using the forkJoin operator to combine these two observables
+          // into a single observable that emits both values as an array.
+          this.posts = posts.map((post, index) => ({
+            id: index,
+            title: post.title,
+            author: user.name,
+            body: 'body',
+          }));
+        })
+      );
+
       loadingCtr.dismiss();
     } catch (error) {
       console.log(error);
@@ -156,16 +179,19 @@ export class HomePage implements OnDestroy{
     this.posts = [];
     const loadingCtr = await this.showLoading('');
     try {
-      zip([userObservable, postObservable]).subscribe(([user, posts]) => {
-        // using the zip operator to combine these two observables into a single observable
-        // that emits an array containing the latest values from both observables.
-        this.posts = posts.map((post, index) => ({
-          id: index,
-          title: post.title,
-          author: user.name,
-          body: 'body',
-        }));
-      });
+      this.subscriptions.push(
+        zip([userObservable, postObservable]).subscribe(([user, posts]) => {
+          // using the zip operator to combine these two observables into a single observable
+          // that emits an array containing the latest values from both observables.
+          this.posts = posts.map((post, index) => ({
+            id: index,
+            title: post.title,
+            author: user.name,
+            body: 'body',
+          }));
+        })
+      );
+
       loadingCtr.dismiss();
     } catch (error) {
       console.log(error);
@@ -194,26 +220,31 @@ export class HomePage implements OnDestroy{
     this.posts = [];
     const loadingCtr = await this.showLoading('');
     try {
-      combineLatest([userObservable, postObservable]).subscribe(
-        ([user, posts]) => {
-          // using the combineLatest operator to combine these two observables
-          // into a single observable that emits an array containing the latest values from both observables.
-          this.posts = posts.map((post, index) => ({
-            id: index,
-            title: post.title,
-            author: user.name,
-            body: 'body',
-          }));
-        }
+      this.subscriptions.push(
+        combineLatest([userObservable, postObservable]).subscribe(
+          ([user, posts]) => {
+            // using the combineLatest operator to combine these two observables
+            // into a single observable that emits an array containing the latest values from both observables.
+            this.posts = posts.map((post, index) => ({
+              id: index,
+              title: post.title,
+              author: user.name,
+              body: 'body',
+            }));
+          }
+        )
       );
+
     } catch (error) {
       console.log(error);
     } finally {
       loadingCtr.dismiss();
     }
-
   }
 
+  /**
+   * merge() is a static method of the Observable class.
+   */
   async callMergeExample() {
     console.log('callMergeExample');
 
@@ -227,25 +258,28 @@ export class HomePage implements OnDestroy{
     this.posts = [];
     const loadingCtr = await this.showLoading('');
     try {
-      merge(userObservable, postObservable).subscribe((data) => {
-        let posts: Post[] = [];
-        let user: User;
+      this.subscriptions.push(
+        merge(userObservable, postObservable).subscribe((data) => {
+          let posts: Post[] = [];
+          let user: User;
 
-        if (Array.isArray(data)) {
-          posts = data;
-          //this.posts = posts.map((post: Post) => ({ title: post.title, author: user.name }));
-        } else {
-          // handle user or post data
-          user = data;
-        }
+          if (Array.isArray(data)) {
+            posts = data;
+            //this.posts = posts.map((post: Post) => ({ title: post.title, author: user.name }));
+          } else {
+            // handle user or post data
+            user = data;
+          }
 
-        this.posts = posts.map((post, index) => ({
-          id: index,
-          title: post.title,
-          author: user?.name,
-          body: 'body',
-        }));
-      });
+          this.posts = posts.map((post, index) => ({
+            id: index,
+            title: post.title,
+            author: user?.name,
+            body: 'body',
+          }));
+        })
+      );
+
       loadingCtr.dismiss();
     } catch (error) {
       console.log(error);
@@ -258,6 +292,9 @@ export class HomePage implements OnDestroy{
 
   }
 
+  /**
+   * map() is an operator that takes a function as an argument.
+   */
   async callMapExample() {
     console.log('callMapExample');
     const userObservable = this.http.get(
@@ -268,10 +305,13 @@ export class HomePage implements OnDestroy{
 
     const loadingCtr = await this.showLoading('');
     try {
-      userObservable.subscribe((data) => {
-        console.log(data);
-        loadingCtr.dismiss();
-      });
+      this.subscriptions.push(
+        userObservable.subscribe((data) => {
+          console.log(data);
+          loadingCtr.dismiss();
+        })
+      );
+
     } catch (error) {
       console.log(error);
     } finally {
@@ -282,6 +322,9 @@ export class HomePage implements OnDestroy{
     }
   }
 
+  /**
+   * switchMap() is an operator that takes a function as an argument.
+   */
   async callSwitchMapExample() {
     console.log('callSwitchMapExample');
 
@@ -294,10 +337,13 @@ export class HomePage implements OnDestroy{
         return this.http.get(firstFilm);
       }));
 
-      userObservable.subscribe((data) => {
-        console.log(data);
-        loadingCtr.dismiss();
-      });
+      this.subscriptions.push(
+        userObservable.subscribe((data) => {
+          console.log(data);
+          loadingCtr.dismiss();
+        })
+      );
+
     } catch (error) {
       console.log(error);
     } finally {
@@ -309,6 +355,9 @@ export class HomePage implements OnDestroy{
 
   }
 
+  /**
+   * filter() is an operator that takes a predicate function as an argument.
+   */
   async filterCallExample() {
     console.log('filterCallExample');
 
@@ -320,10 +369,13 @@ export class HomePage implements OnDestroy{
         return data.films.length > 0;
       }));
 
-      userObservable.subscribe((data) => {
-        console.log(data);
-        loadingCtr.dismiss();
-      });
+      this.subscriptions.push(
+        userObservable.subscribe((data) => {
+          console.log(data);
+          loadingCtr.dismiss();
+        })
+      );
+
     } catch (error) {
       console.log(error);
     } finally {
@@ -332,9 +384,11 @@ export class HomePage implements OnDestroy{
         loadingCtr_.dismiss();
       }
     }
-
   }
 
+  /**
+   * takeUntil() is an operator that takes an observable as an argument.
+   */
   async takeUntilCallExample() {
     console.log('takeUntilCallExample');
 
@@ -344,10 +398,13 @@ export class HomePage implements OnDestroy{
         'https://swapi.dev/api/people/1'
       ).pipe(takeUntil(this.destroy));
 
-      userObservable.subscribe((data) => {
-        console.log(data);
-        loadingCtr.dismiss();
-      });
+      this.subscriptions.push(
+        userObservable.subscribe((data) => {
+          console.log(data);
+          loadingCtr.dismiss();
+        })
+      );
+
     } catch (error) {
       console.log(error);
     } finally {
@@ -356,9 +413,100 @@ export class HomePage implements OnDestroy{
         loadingCtr_.dismiss();
       }
     }
-
   }
 
+  /**
+   * subject subscribe example
+   */
+  async callSubjectExample() {
+    console.log('callSubjectExample');
+
+    const loadingCtr = await this.showLoading('');
+    try {
+      this.subscriptions.push(
+        this.mySubject.subscribe((data) => {
+          console.log(data);
+          loadingCtr.dismiss();
+        })
+      );
+
+      this.howManyCalled += 1;
+      this.mySubject.next(`called ${this.howManyCalled} times by Subject`);
+      console.log(`called ${this.subscriptions.length} times by Subject.subscribe`);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      const loadingCtr_ = await this.loadingCtrl.getTop();
+      if (loadingCtr_) {
+        loadingCtr_.dismiss();
+      }
+    }
+  }
+
+  /**
+   * behavior subject subscribe example
+   */
+  async callBehaviorSubjectExample() {
+    console.log('callBehaviorSubjectExample');
+
+    const loadingCtr = await this.showLoading('');
+    try {
+      this.subscriptions.push(
+        this.myBehaviorSubject.subscribe((data) => {
+          console.log(data);
+          loadingCtr.dismiss();
+        })
+      );
+
+      this.howManyCalledBehaviorSubject += 1;
+      this.myBehaviorSubject.next(`called ${this.howManyCalledBehaviorSubject} times by BehaviorSubject`);
+      console.log(`called ${this.subscriptions.length} times by BehaviorSubject.subscribe`);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      const loadingCtr_ = await this.loadingCtrl.getTop();
+      if (loadingCtr_) {
+        loadingCtr_.dismiss();
+      }
+    }
+  }
+
+  async callReplaySubjectExample() {
+    console.log('callReplaySubjectExample');
+
+    const loadingCtr = await this.showLoading('');
+    try {
+      this.subscriptions.push(
+        this.myReplaySubject.subscribe((data) => {
+          console.log(data);
+          loadingCtr.dismiss();
+        })
+      );
+
+      for(let i = 0; i < 3; i++) {
+        this.howManyCalledReplaySubject += 1;
+        this.myReplaySubject.next(`called ${this.howManyCalledReplaySubject} times by ReplaySubject`);
+      }
+
+      console.log(`called ${this.subscriptions.length}: times by ReplaySubject.subscribe`);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      const loadingCtr_ = await this.loadingCtrl.getTop();
+      if (loadingCtr_) {
+        loadingCtr_.dismiss();
+      }
+    }
+  }
+
+  callDefaultExample() {
+    console.log('callDefaultExample');
+  }
+
+  /**
+   * call rxjs function
+   * @param e
+   */
   callFunction(e: any) {
     console.log(e.target.value);
     const functionName = e.target.value;
@@ -385,10 +533,19 @@ export class HomePage implements OnDestroy{
       case 'takeUntil':
         this.takeUntilCallExample();
         break;
+      case 'subject':
+        this.callSubjectExample();
+        break;
+      case 'behaviorSubject':
+        this.callBehaviorSubjectExample();
+        break;
+      case 'replaySubject':
+        this.callReplaySubjectExample();
+        break;
       default:
+        this.callDefaultExample();
         break;
     }
-
   }
 
 
